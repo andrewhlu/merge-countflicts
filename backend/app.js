0e4a7e0d946f4616cf929dce59a5c00036c48f61
waqar.kalim <waqaar.199@gmail.com>
2021-01-16 17:29:57 -0500

    Added database config and it connects, and added game logic by using maps instead of tables which needs to changed

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var fs = require("fs");
+var dotenv = require("dotenv");
+
+var pg = require("pg");
 
 const http = require("http");
 const socketIo = require("socket.io");
@@ -14,6 +18,22 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+dotenv.config();
+
+const pgConfig = {
+  user: "cabbage",
+  password: process.env.CRDB_PWD,
+  host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+  database: 'dark-walrus-177.defaultdb',
+  port: 26257,
+  ssl: {
+    ca: fs.readFileSync('cc-ca.crt').toString()
+  }
+};
+
+const pgClient = new pg.Client(pgConfig);
+pgClient.connect();
+
 const responseArray = [];
 
 const port = process.env.PORT || 3001;
@@ -23,10 +43,10 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(cors());
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -57,7 +77,15 @@ const io = socketIo(server, {
   },
 });
 
+const timeElapsedBetweenButtonPresses = 300;
+const roomCountMap = {};
+const roomDataMap = {};
+
+
+
 let interval;
+
+// var count = 0;
 
 io.on("connection", (socket) => {
   console.log("New client connected");
@@ -72,30 +100,69 @@ io.on("connection", (socket) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("room", (room) => {
+  socket.on("room", async (room) => {
     socket.join(room);
+
+    try {
+      await pgClient.query(`select * from ${room};`);
+      console.log(`${room} exists!`);
+    } catch (e) {
+      if (e?.routine === "NewUndefinedRelationError") {
+        await pgClient.query(`create table ${room} (numPushed integer primary key, name text)`);
+        console.log(`${room} created!`);
+      }
+    }
+    // if (roomCountMap[room]) {
+    //   io.sockets.in(room).emit("count", roomCountMap[room]);
+    // } else {
+    //   roomCountMap[room] = 0;
+    //   io.sockets.in(room).emit("count", roomCountMap[room]);
+    // }
+
+    if (!(room in roomCountMap)) {
+      roomCountMap[room] = 0;
+    }
+    io.sockets.in(room).emit("count", roomCountMap[room]);
+
     console.log(`Joined Room ${room}`);
-  })
-})
+  });
+});
+
 
 io.on("connection", (socket) => {
   socket.on("buttonPress", (data) => {
     if (data.room) {
-      const mostRecentButtomPress = responseArray[responseArray.length - 1];
-      responseArray.push(data);
+      if (!(data.room in roomDataMap)) {
+        roomDataMap[data.room] = [];
+        console.log(roomDataMap);
+      }
+
+      const mostRecentButtomPress =
+        roomDataMap[data.room][roomDataMap[data.room].length - 1];
+
+      roomDataMap[data.room].push(data);
       socket.in(data.room).emit(data);
 
-      if ((mostRecentButtomPress) && (data.room === mostRecentButtomPress.room) && (data.timestamp - mostRecentButtomPress.timestamp < 300)) {
-        // socket.in(data.room).emit("reset", true);
-        socket.emit("reset", true);
+      if (
+        (mostRecentButtomPress !== undefined &&
+          data.room === mostRecentButtomPress.room &&
+          data.timestamp - mostRecentButtomPress.timestamp < timeElapsedBetweenButtonPresses) ||
+        (mostRecentButtomPress !== undefined && data.room === mostRecentButtomPress.room &&
+          mostRecentButtomPress.id === data.id)
+      ) {
+        // If the users messed up
+        roomCountMap[data.room] = 0;
+        roomDataMap[data.room] = [];
+        io.sockets.in(data.room).emit("count", roomCountMap[data.room]);
         console.log("You done messed up!");
       } else {
-        socket.emit("reset", false);
+        // If they didn't mess up
+        roomCountMap[data.room] = roomCountMap[data.room] + 1;
+        io.sockets.in(data.room).emit("count", roomCountMap[data.room]);
       }
-      console.log(data);
     }
-  })
-})
+  });
+});
 
 const getApiAndEmit = (socket) => {
   const response = new Date();
@@ -106,3 +173,4 @@ const getApiAndEmit = (socket) => {
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
+// export default app;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,179 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import {
-//   Checkbox,
-//   FormGroup,
-//   FormControlLabel,
-//   TextField,
-//   Button,
-//   Typography,
-// } from "@material-ui/core";
-
-// // import ClientComponent from "./ClientComponent";
-
-// import socketIOClient from "socket.io-client";
-// // import openSocket from "socket.io-client";
-// const ENDPOINT = "http://192.168.4.26:3001";
-
-// function uuidv4() {
-//   return "xxxxxxxx".replace(/[xy]/g, function (c) {
-//     var r = (Math.random() * 16) | 0,
-//       v = c === "x" ? r : (r & 0x3) | 0x8;
-//     return v.toString(16);
-//   });
-// }
-
-// function Main() {
-//   const [room, setRoom] = useState("");
-//   const [count, setCount] = useState(0);
-//   const [loadClient, setLoadClient] = useState(true);
-
-//   const [createRoomChecked, setCreateRoomChecked] = useState(false);
-//   const [joinRoomChecked, setJoinRoomChecked] = useState(false);
-
-//   const [roomIdGenerated, setRoomIdGenerated] = useState(false);
-
-//   const [startGame, setStartGame] = useState(false);
-
-//   const [response, setResponse] = useState("");
-//   const [socket, setSocket] = useState();
-
-//   useEffect(() => {
-//     setSocket(socketIOClient(ENDPOINT, {transport: ['websocket']}));
-//     socket.on("FromAPI", data => {
-//       setResponse(data);
-//     });
-
-//     return () => socket.disconnect();
-//   }, []);
-
-//   const joinRoom = (room) => {
-//     const socket = socketIOClient(ENDPOINT, {transport: ['websocket']});
-//     socket.on('connection', socket)
-//   }
-
-
-//   return (
-//     <>
-//       {!startGame ? (
-//         <>
-//           <FormGroup row>
-//             <FormControlLabel
-//               control={
-//                 <Checkbox
-//                   checked={createRoomChecked}
-//                   onChange={() => {
-//                     if (!createRoomChecked && joinRoomChecked) {
-//                       setJoinRoomChecked(!joinRoomChecked);
-//                     }
-//                     setCreateRoomChecked(!createRoomChecked);
-//                   }}
-//                 />
-//               }
-//               label="Create Room"
-//             />
-//             <FormControlLabel
-//               control={
-//                 <Checkbox
-//                   checked={joinRoomChecked}
-//                   onChange={() => {
-//                     if (createRoomChecked && !joinRoomChecked) {
-//                       setCreateRoomChecked(!createRoomChecked);
-//                     }
-//                     setJoinRoomChecked(!joinRoomChecked);
-//                   }}
-//                 />
-//               }
-//               label="Join Room"
-//             />
-//           </FormGroup>
-//           {createRoomChecked ? (
-//             <>
-//               <Button
-//                 onClick={() => {
-//                   setRoom(uuidv4());
-//                   setRoomIdGenerated(true);
-//                   setStartGame(true);
-//                 }}
-//                 variant="contained"
-//                 color="primary"
-//               >
-//                 Generate Room Id
-//               </Button>
-//             </>
-//           ) : null}
-//           {joinRoomChecked ? (
-//             <>
-//               <TextField
-//                 value={room}
-//                 autoFocus={true}
-//                 helperText={"Enter Room Id Here..."}
-//                 onChange={(event) => {
-//                   setRoom(event.target.value);
-//                 }}
-//               />
-//               <Button
-//                 onClick={() => {
-//                   setStartGame(true);
-//                 }}
-//                 variant="contained"
-//                 color="primary"
-//               >
-//                 Join Room
-//               </Button>
-//             </>
-//           ) : null}
-
-//           {roomIdGenerated ? (
-//             <>
-//               <Typography>{room}</Typography>
-//               {/* <TextField
-//           value={room}
-//           disabled={true}
-//         /> */}
-//             </>
-//           ) : null}
-//         </>
-//       ) : null}
-//       {startGame ? (
-//         <>
-//           <Typography>{`Room Id: ${room}`}</Typography>
-//           <Typography variant="h1">{count}</Typography>
-//           <Button
-//             type="submit"
-//             variant="contained"
-//             color="primary"
-//             onClick={() => {
-//               setCount(count + 1);
-//             }}
-//           >
-//             Press Me
-//           </Button>
-//         </>
-//       ) : null}
-//     </>
-//     // {/* <Typography variant="h1">{count}</Typography>
-//     // <Button
-//     //   type="submit"
-//     //   variant="contained"
-//     //   color="primary"
-//     //   onClick={() => {
-//     //     setCount(count + 1);
-//     //   }}
-//     // >
-//     //   Press Me
-//     // </Button> */}
-//     // {/* LOAD OR UNLOAD THE CLIENT */}
-//     // {/* <button onClick={() => setLoadClient((prevState) => !prevState)}>
-//     //   STOP CLIENT
-//     // </button> */}
-//     // {/* SOCKET IO CLIENT*/}
-//     // {/* {loadClient ? <ClientComponent /> : null} */}
-//   );
-// }
-
-// export default Main;
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Checkbox,
   FormGroup,
@@ -197,11 +22,19 @@ function uuidv4() {
   });
 }
 
+function generateUniqueNumber() {
+  return "xxxxxxxxxxxxxxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      room: '',
+      room: "",
       count: 0,
       loadClient: true,
       createRoomChecked: false,
@@ -209,19 +42,21 @@ class Main extends Component {
       roomIdGenerated: false,
       startGame: false,
       response: "",
-      socketIO: null
-    }
+      socketIO: null,
+      id: null,
+    };
   }
 
   componentDidMount() {
-    this.setState({socketIO: socketIOClient(ENDPOINT, {transport: ['websocket']})}, () => {
-      this.state.socketIO.on("reset", data => {
-        if (data === true) {
-          this.setState({count: 0});
-        } else {
-          this.setState({count: this.state.count+1});
+    this.setState({ id: generateUniqueNumber() }, () => {
+      this.setState(
+        { socketIO: socketIOClient(ENDPOINT, { transport: ["websocket"] }) },
+        () => {
+          this.state.socketIO.on("count", (data) => {
+            this.setState({ count: data });
+          });
         }
-      });
+      );
     });
 
     return () => this.socketIO.disconnect();
@@ -229,138 +64,150 @@ class Main extends Component {
 
   joinRoom = () => {
     const { room, socketIO } = this.state;
-    socketIO.emit('room', room);
-    this.setState({startGame: true});
-  }
+    socketIO.emit("room", room);
+    this.setState({ startGame: true });
+  };
 
   handleButtonPress = () => {
-    const { count, room, socketIO } = this.state;
+    const { id, count, room, socketIO } = this.state;
     // this.setState({ count: count+1});
 
     const data = {
+      id: id, 
       room: room,
       message: "Hello World",
-      timestamp: Date.now()
+      timestamp: Date.now(),
     };
     console.log(data);
-    socketIO.emit('buttonPress', data)      
-  }
-
-
+    socketIO.emit("buttonPress", data);
+  };
 
   render() {
-    const {room, count, loadClient, createRoomChecked, joinRoomChecked, roomIdGenerated, startGame, response, socketIO} = this.state;
-    return (<>
-    {/* <Typography variant="h1">Addiction With Extra Steps</Typography> */}
-      {!startGame ? (
-        <>
-          <FormGroup row>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={createRoomChecked}
-                  onChange={() => {
-                    if (!createRoomChecked && joinRoomChecked) {
-                      this.setState({joinRoomChecked: !joinRoomChecked});
-                    }
-                    this.setState({createRoomChecked: !createRoomChecked});
-                  }}
-                />
-              }
-              label="Create Room"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={joinRoomChecked}
-                  onChange={() => {
-                    if (createRoomChecked && !joinRoomChecked) {
-                      this.setState({createRoomChecked: !createRoomChecked})
-                      // setCreateRoomChecked(!createRoomChecked);
-                    }
-                    this.setState({joinRoomChecked: !joinRoomChecked})
-                    // setJoinRoomChecked(!joinRoomChecked);
-                  }}
-                />
-              }
-              label="Join Room"
-            />
-          </FormGroup>
-          {createRoomChecked ? (
-            <>
-              <Button
-                onClick={() => {
-                  this.setState({room: uuidv4()}, () => {
-                    this.setState({roomIdGenerated: true}, () => {
-                      this.joinRoom();
-                      // this.setState({startGame: true});
-                    })
-                  });
-                }}
-                variant="contained"
-                color="primary"
-              >
-                Generate Room Id
-              </Button>
-            </>
-          ) : null}
-          {joinRoomChecked ? (
-            <>
-              <TextField
-                value={room}
-                autoFocus={true}
-                helperText={"Enter Room Id Here..."}
-                onChange={(event) => {
-                  this.setState({room: event.target.value});
-                  // setRoom(event.target.value);
-                }}
+    const {
+      room,
+      count,
+      loadClient,
+      createRoomChecked,
+      joinRoomChecked,
+      roomIdGenerated,
+      startGame,
+      response,
+      socketIO,
+    } = this.state;
+    return (
+      <>
+        {/* <Typography variant="h1">Addiction With Extra Steps</Typography> */}
+        {!startGame ? (
+          <>
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={createRoomChecked}
+                    onChange={() => {
+                      if (!createRoomChecked && joinRoomChecked) {
+                        this.setState({ joinRoomChecked: !joinRoomChecked });
+                      }
+                      this.setState({ createRoomChecked: !createRoomChecked });
+                    }}
+                  />
+                }
+                label="Create Room"
               />
-              <Button
-                onClick={() => {
-                  this.joinRoom();
-                  this.setState({startGame: true})
-                  // setStartGame(true);
-                }}
-                variant="contained"
-                color="primary"
-              >
-                Join Room
-              </Button>
-            </>
-          ) : null}
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={joinRoomChecked}
+                    onChange={() => {
+                      if (createRoomChecked && !joinRoomChecked) {
+                        this.setState({
+                          createRoomChecked: !createRoomChecked,
+                        });
+                        // setCreateRoomChecked(!createRoomChecked);
+                      }
+                      this.setState({ joinRoomChecked: !joinRoomChecked });
+                      // setJoinRoomChecked(!joinRoomChecked);
+                    }}
+                  />
+                }
+                label="Join Room"
+              />
+            </FormGroup>
+            {createRoomChecked ? (
+              <>
+                <Button
+                  onClick={() => {
+                    this.setState({ room: uuidv4() }, () => {
+                      this.setState({ roomIdGenerated: true }, () => {
+                        this.joinRoom();
+                        // this.setState({startGame: true});
+                      });
+                    });
+                  }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Generate Room Id
+                </Button>
+              </>
+            ) : null}
+            {joinRoomChecked ? (
+              <>
+                <TextField
+                  value={room}
+                  autoFocus={true}
+                  helperText={"Enter Room Id Here..."}
+                  onChange={(event) => {
+                    this.setState({ room: event.target.value });
+                    // setRoom(event.target.value);
+                  }}
+                />
+                <Button
+                  onClick={() => {
+                    this.joinRoom();
+                    this.setState({ startGame: true });
+                    // setStartGame(true);
+                  }}
+                  variant="contained"
+                  color="primary"
+                >
+                  Join Room
+                </Button>
+              </>
+            ) : null}
 
-          {roomIdGenerated ? (
-            <>
-              <Typography>{room}</Typography>
-              {/* <TextField
+            {roomIdGenerated ? (
+              <>
+                <Typography>{room}</Typography>
+                {/* <TextField
           value={room}
           disabled={true}
         /> */}
-            </>
-          ) : null}
-        </>
-      ) : null}
-      {startGame ? (
-        <>
-          <Typography>{`Room Id: ${room}`}</Typography>
-          <Typography variant="h1">{count}</Typography>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              this.handleButtonPress();
-              // this.setState({count: count+1});
-              // setCount(count + 1);
-            }}
-          >
-            Press Me
-          </Button>
-        </>
-      ) : null}
-    </>)
-  };
-
+              </>
+            ) : null}
+          </>
+        ) : null}
+        {startGame ? (
+          <>
+            <Typography>{`Room Id: ${room}`}</Typography>
+            <Typography variant="h1">{count}</Typography>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                this.handleButtonPress();
+                // this.setState({count: count+1});
+                // setCount(count + 1);
+              }}
+            >
+              Press Me
+            </Button>
+          </>
+        ) : null}
+      </>
+    );
+  }
 }
 
 export default Main;
